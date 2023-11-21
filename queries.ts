c883f9115cac509f9e815eeb7f29b75aae3873e3
tannazma <97254921+tannazma@users.noreply.github.com>
2023-11-21 12:28:49 +0100

    create schema relation for pet and owner: every pet can have one owner and every owner can have multiple pets + create newPet in queries

--- queries.ts
+++ queries.ts
@@ -1,1 +1,23 @@
-console.log("hbbhb")
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+const runQuery = async () => {
+  try {
+    const newPet = await prisma.pet.create({
+      data: {
+        name: "Porky",
+        kind: "Pig",
+        owner: {
+          connect: {
+            id: 1,
+          },
+        },
+      },
+    });
+    console.log(newPet);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+runQuery();
