4481b25aa3fed3331a29d67f7a705b63ab4d098d
tannazma <97254921+tannazma@users.noreply.github.com>
2023-11-21 11:56:40 +0100

    creating relation data

---
+++ prisma/seed.ts
@@ -0,0 +1,33 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+import pets from "../pet.json";
+import owners from "../owner.json";
+
+const seed = async () => {
+  for (let i = 0; i < pets.length; i += 1) {
+    const petData = pets[i];
+    if (petData) {
+      await prisma.pet.create({ data: petData });
+    }
+  }
+
+  //   seedData.pets.forEach(async (pet) => {
+  //     const { kind, name } = pet;
+  //     const insertPet = await prisma.pet.create({
+  //       data: {
+  //         name,
+  //         kind,
+  //       },
+  //     });
+  //   });
+  for (let i = 0; i < owners.length; i+=1) {
+    const ownerData = owners[i];
+    if (ownerData) {
+    }
+    await prisma.owner.create({
+      data: ownerData,
+    });
+  }
+};
+seed();
